0e8e5a83b7e0c793ab88c3cf4a64f79c9f4e0260
SANCHEON <89966110+SANCHEON@users.noreply.github.com>
2022-08-22 19:50:10 +0900

    ㅂㅂ

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("기숙사수용현황분석.csv")
+
+col1, col2, col3 = st.columns(3)
+col1.metric("전국 대학 수", "70 °F", "1.2 °F")
+col2.metric("국공립", "9 mph", "-8%")
+col3.metric("사립", "86%", "4%")
+
 st.dataframe(data)
 
-st.write("Hello World")
