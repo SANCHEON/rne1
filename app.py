42490d516a6b0c4c02762031d7090a5e5f78828c
SANCHEON <89966110+SANCHEON@users.noreply.github.com>
2022-08-22 20:02:37 +0900

    qq

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 data = pd.read_csv("기숙사수용현황분석.csv")
 num1 = len(data['학교'].unique())
 
-num2 = len(data[['설립구분'] != '사립']['학교'].unique())
+num2 = len(data[data['설립구분'] != '사립']['학교'].unique())
 num3 = num1 - num2
 
 col1, col2, col3 = st.columns(3)
