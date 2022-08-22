8d0f9058bef93b5469895aba66784435a189d060
SANCHEON <89966110+SANCHEON@users.noreply.github.com>
2022-08-22 19:46:06 +0900

    qq

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("기숙사수용현황분석.csv")
+data = pd.read_csv("/기숙사수용현황분석.csv")
 st.dataframe(data)
 
 st.write("Hello World")
