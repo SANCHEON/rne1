2651a029bbdb81152f09adae53c6de83c376459c
SANCHEON <89966110+SANCHEON@users.noreply.github.com>
2022-08-22 19:44:25 +0900

    aa

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("/content/drive/MyDrive/기숙사수용현황분석.csv")
+st.dataframe(data)
+
 st.write("Hello World")
